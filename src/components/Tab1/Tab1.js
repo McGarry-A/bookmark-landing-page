b8b96404102b2621754e04e99865f650e18bc462
Ahmed <atomcgarry@gmail.com>
2022-01-08 00:04:19 +0000

    fixing some styles

--- src/components/Tab1/Tab1.js
+++ src/components/Tab1/Tab1.js
@@ -8,7 +8,7 @@ const Tab1 = () => {
           <div className="background-blue"></div>
       </div>
       <div className="tab-right">
-        <h2 className="tab-right-title">Bookmark in One Click</h2>
+        <h2 className="tab-right-title">Bookmark in one click</h2>
         <p className="tab-right-text">Organise your bookmarks however you like. Our simple drag and drop interface gives you complete control over how you manage your favorite sites.</p>
         <button className="tab-right-button">More Info</button>
       </div>
