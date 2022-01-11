c34b9248c9c75c037df79d0a68ebda0a67b955a2
Ahmed <atomcgarry@gmail.com>
2022-01-11 17:16:14 +0000

    navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,52 @@
-import Hamburger from "../../images/icon-hamburger.svg"
-import {ReactComponent as Logo} from "../../images/logo-bookmark.svg"
-import "./Navbar.css"
+import Hamburger from "../../images/icon-hamburger.svg";
+import { ReactComponent as Exit } from "../../images/icon-close.svg";
+import { ReactComponent as Logo } from "../../images/logo-bookmark.svg";
+import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <header>
-            <div className="logo-div">
-                <Logo color="black"/>
-            </div>
-            <nav>
-                <ul className="nav-list">
-                    <li className="nav-item">Features</li>
-                    <li className="nav-item">Pricing</li>
-                    <li className="nav-item">Contact</li>
-                    <li className="nav-item"><button className="nav-cta">Login</button></li>
-                    <li><img className="nav-hamburger" src={Hamburger} alt="hamburger"/></li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+  const toggleNav = () => {
+    document.querySelector(".nav-hamburger").classList.toggle("hidden");
 
-export default Navbar
+    const navList = document.querySelector(".nav-list");
+    navList.style.display === "flex"
+      ? (navList.style.display = "none")
+      : (navList.style.display = "flex");
+
+    console.log("clicked");
+  };
+  return (
+    <header>
+      <div className="logo-div">
+        <Logo color="black" />
+      </div>
+      <nav>
+        <ul className="nav-list mobile-nav-list">
+          <li className="mobile-nav-item exit-button">
+            <Exit
+              onClick={toggleNav}
+              className="nav-exit"
+              alt="hamburger"
+              fill="white"
+            />
+          </li>
+          <li className="nav-item mobile-nav-item nav-link">Features</li>
+          <li className="nav-item mobile-nav-item nav-link">Pricing</li>
+          <li className="nav-item mobile-nav-item nav-link">Contact</li>
+          <li className="nav-item mobile-nav-item">
+            <button className="nav-cta">Login</button>
+          </li>
+        </ul>
+        <div className="mobile-nav">
+          <img
+            onClick={toggleNav}
+            className="nav-hamburger"
+            src={Hamburger}
+            alt="hamburger"
+          />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
